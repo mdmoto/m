84ea00ef46735213988f24c243b81f08eda10fb8
Adam Maollar <adam@maollar.com>
2026-03-20 08:58:24 +0900

    deploy: fix H5 navigation fallback

--- assets/passport.085bda70.js
+++
@@ -1,1 +0,0 @@
-import{aw as a,ax as s}from"./index-046bf061.js";async function o(o){return a({url:"passport/login/save/biology",method:s.POST,params:o})}export{o as s};
---
+++ assets/passport.8942c368.js
@@ -0,0 +1,1 @@
+import{aw as a,ax as s}from"./index-d1e18c35.js";async function o(o){return a({url:"passport/login/save/biology",method:s.POST,params:o})}export{o as s};
