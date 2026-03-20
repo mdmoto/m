c0db9dbae8d6ec180dddedaf7c0e8aa8e8b2b16c
Adam Maollar <adam@maollar.com>
2026-03-20 09:05:25 +0900

    deploy: update H5 build with vite.config fix, node_modules patches, and diagnostic logs

---
+++ assets/pages-mine-distribution-achievement.462e2237.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./index-8fc01dab.js";const r=n({},[["render",function(n,r,e,t,o,s){return null}]]);export{r as default};
--- assets/pages-mine-distribution-achievement.81ac5218.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./index-d1e18c35.js";const r=n({},[["render",function(n,r,e,t,o,s){return null}]]);export{r as default};
