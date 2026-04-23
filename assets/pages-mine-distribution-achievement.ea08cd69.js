60d2c2d21c7e6519eaf7f586ce11ec83c0a9c6db
Adam Maollar <adam@maollar.com>
2026-04-23 14:41:55 +0700

    deploy: updated H5 build with login crash fix and AI-friendly bypass

---
+++ assets/pages-mine-distribution-achievement.ea08cd69.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./index-56122844.js";const r=n({},[["render",function(n,r,e,t,o,s){return null}]]);export{r as default};
--- assets/passport.ad875982.js
+++
@@ -1,1 +0,0 @@
-import{at as a,au as s}from"./index-168e450a.js";async function o(o){return a({url:"passport/login/save/biology",method:s.POST,params:o})}export{o as s};
