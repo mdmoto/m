e796071e1d9263582b8e8b123cf73bb85132be01
Adam Maollar <adam@maollar.com>
2026-04-23 14:12:58 +0700

    deploy: update H5 build artifacts (AI-friendly login and logout fix)

--- assets/pages-mine-distribution-achievement.6d3c6ef6.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./index-2c2a0b06.js";const r=n({},[["render",function(n,r,e,t,o,s){return null}]]);export{r as default};
--- assets/passport.5e7c1bc3.js
+++
@@ -1,1 +0,0 @@
-import{at as a,au as s}from"./index-2c2a0b06.js";async function o(o){return a({url:"passport/login/save/biology",method:s.POST,params:o})}export{o as s};
